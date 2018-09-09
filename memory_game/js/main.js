fd649795ee171b161c4001973ed27047df1f3340
Daniel Becker <danielbecker@Daniels-MacBook-Pro.local>
2018-09-09 13:27:21 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
-console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "king";
-var CardThree = "Queen";
-var cardFour = "King";
-console.log("User Flipped" + cardOne);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen")
+var cardTwo = cards[1];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king")
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("Congrats you found a match!")
+	} else alert("Sorry try again")
+
+}
